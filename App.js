5337e05a4e526bbb316c93abcfaa5227c22ff338
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2023-05-04 17:16:00 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
 import * as Font from "expo-font";
 
 import BottomTabNavigator from "./components/BottomTabNavigator";
-
+ 
 export default class App extends Component {
   constructor() {
     super();
